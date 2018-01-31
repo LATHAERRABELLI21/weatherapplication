65227e2e9c50093768beca762df61fea0532df61
LATHAERRABELLI21 <lathaerrabelli@gmail.com>
2018-01-31 11:05:55 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpModule} from '@angular/http';
+import {WeatherService} from './weather.service';
+import { AppComponent } from './app.component';
+import { SettingComponent } from './setting/setting.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const appRoutes=[
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:'setting' , component:SettingComponent
+  },
+  {
+    path:'home',component:HomeComponent
+  }
+
+];
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SettingComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule
+  ],
+  providers: [WeatherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
